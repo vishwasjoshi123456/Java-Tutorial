9980e75642c38da6f4a2288c2d68349090e30fc9
Vishwas Joshi <vishwasjoshi123456@gmail.com>
2022-08-26 14:46:22 +0530

    Third Commit

---
+++ Datatype_Boolean.java
@@ -0,0 +1,27 @@
+public class Datatype_Boolean {
+// Java Program to Demonstrate Boolean Primitive DataType
+
+	// Main driver method
+	public static void main(String args[])
+	{
+
+		//Boolean data type is a data type that has one of two possible values (usually denoted true and false).
+		// Setting boolean to false and true initially
+		boolean a = false;
+		boolean b = true;
+
+		// If condition holds
+		if (b == true){
+
+			// Print statement
+			System.out.println("Hi Geek");
+		}
+		// If condition holds
+		if(a == false){
+		
+		// Print statement
+			System.out.println("Hello Geek");		
+		}
+	}
+
+}
---
+++ Datatype_Float.java
@@ -0,0 +1,16 @@
+
+public class Datatype_Float {
+// Java Program to Illustrate Float Primitive Data Type
+// Main driver method
+	public static void main(String[] args)
+	{
+		// Declaring and initializing float value
+		// float value1 = 9.87;
+		// Print statement
+		// System.out.println(value1);
+		float value2 = 9.87f;
+		System.out.println(value2);
+	}
+
+
+}
